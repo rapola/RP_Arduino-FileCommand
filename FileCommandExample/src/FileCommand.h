2da63190666e0a28f272b7bd0273f597e420f196
Ralf <59847860+rapola@users.noreply.github.com>
2021-10-26 19:20:58 +0200

    Update FileCommand.h

--- FileCommandExample/src/FileCommand.h
+++ FileCommandExample/src/FileCommand.h
@@ -2,11 +2,11 @@
  * SerialCommand - A Wiring/Arduino library to tokenize and parse commands
  * received over a serial port.
  * 
+ * Copyright (C) 2021 rapola
  * Copyright (C) 2012 Stefan Rado
  * Copyright (C) 2011 Steven Cogswell <steven.cogswell@gmail.com>
  *                    http://husks.wordpress.com
- * 
- * Version 20120522
+ *
  * 
  * This library is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
